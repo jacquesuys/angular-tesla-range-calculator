250adaf6f5c731a6acb665d46e3c1909ef0184f2
Jacques Uys <jaxuys@gmail.com>
2017-07-12 16:12:21 +0200

    Climate control

--- src/app/tesla-battery/tesla-battery.module.ts
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -14,6 +14,7 @@ import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.
 import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
 import { TeslaStatsComponent } from './components/tesla-stats/tesla-stats.component';
 import { TeslaCounterComponent } from './components/tesla-counter/tesla-counter.component';
+import { TeslaClimateComponent } from './components/tesla-climate/tesla-climate.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { TeslaCounterComponent } from './components/tesla-counter/tesla-counter.
     TeslaBatteryComponent,
     TeslaCarComponent,
     TeslaStatsComponent,
-    TeslaCounterComponent
+    TeslaCounterComponent,
+    TeslaClimateComponent
   ],
   imports: [
     CommonModule,
