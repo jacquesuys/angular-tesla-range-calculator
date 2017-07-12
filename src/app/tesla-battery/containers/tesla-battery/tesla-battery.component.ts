35140164cb04fbd11cf6f215d6e2dbecd4031132
Jacques Uys <jaxuys@gmail.com>
2017-07-12 14:48:39 +0200

    Tesla stats

--- src/app/tesla-battery/containers/tesla-battery/tesla-battery.component.ts
+++ src/app/tesla-battery/containers/tesla-battery/tesla-battery.component.ts
@@ -3,6 +3,8 @@
  */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Stat } from '../../models/stat.interface';
+import { BatteryService } from '../../tesla-battery.service';
 
 @Component({
   selector: 'tesla-battery',
@@ -10,6 +12,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
     <form class="tesla-battery" [formGroup]="tesla">
       <h1>{{ title }}</h1>
       <tesla-car [wheelsize]="tesla.get('config.wheels').value"></tesla-car>
+      <tesla-stats [stats]="stats"></tesla-stats>
       <div class="tesla-battery__notice">
         <p>
           The actual amount of range that you experience will vary based
@@ -29,10 +32,17 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class TeslaBatteryComponent implements OnInit {
 
   title: string = 'Range Per Charge';
+  models: any;
+  stats: Stat[];
   tesla: FormGroup;
-  
-  constructor(public fb: FormBuilder) {}
+
+  private results: Array<String> = ['60', '60D', '75', '75D', '90D', 'P100D'];
+
+  constructor(public fb: FormBuilder, private batteryService: BatteryService) {}
   ngOnInit() {
+
+    this.models = this.batteryService.getModelData();
+
     this.tesla = this.fb.group({
       config: this.fb.group({
         speed: 55,
@@ -40,7 +50,21 @@ export class TeslaBatteryComponent implements OnInit {
         climate: true,
         wheels: 19
       })
-  });
-}
+    });
+
+    this.stats = this.calculateStats(this.results, this.tesla.controls['config'].value);
+
+  }
+
+  private calculateStats(models, value): Stat[]  {
+    return models.map(model => {
+      const { speed, temperature, climate, wheels } = value;
+      const miles = this.models[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+      return {
+        model,
+        miles
+      };
+    });
+  }
 
 }
